ed81616f4ecb73c24991c14429087d280a6b0ec2
harmel3056 <melh1988@hotmail.com>
2026-04-22 15:27:42 +1000

    Bring existing repo into line with updated service logic, still pending SQLite

--- app/persistence/in_memory/result_repository.py
+++ app/persistence/in_memory/result_repository.py
@@ -2,8 +2,23 @@ from app.persistence.interfaces.result_repository import ResultRepository
 
 class InMemoryResultRepository(ResultRepository):
     def __init__(self):
-        self._store = []
+        # key: (student_number, test_id)
+        # value: TestResult domain object
+        self._store = {}
 
     def save(self, result):
-        self._store.append(result)
+        key = (result.student_number, result.test_id)
+        self._store[key] = result
         return result
+
+    def get_by_student_and_test(self, student_number, test_id):
+        return self._store.get((student_number, test_id))
+
+    def get_by_test_id(self, test_id):
+        results = []
+        
+        for (student, stored_test_id), result in self._store.items():
+            if stored_test_id == test_id:
+                results.append(result)
+
+        return results
--- app/persistence/interfaces/result_repository.py
+++ app/persistence/interfaces/result_repository.py
@@ -1,3 +1,15 @@
+from app.models.domain_models import TestResult
+
 class ResultRepository:
-    def save (self, result):
+    def save(self, result: TestResult) -> TestResult:
+        raise NotImplementedError
+
+    def get_by_student_and_test(
+        self,
+        student_number: str,
+        test_id: str
+    ) -> TestResult | None:
+        raise NotImplementedError
+
+    def get_by_test_id(self, test_id: str) -> list[TestResult]:
         raise NotImplementedError
