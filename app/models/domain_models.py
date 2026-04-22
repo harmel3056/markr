b8bd68ba99333e99b02d8109e207eeff1eaad28c
harmel3056 <melh1988@hotmail.com>
2026-04-22 12:20:26 +1000

    Lightweight domain model to model required elements and provide percentage calculation logic

---
+++ app/models/domain_models.py
@@ -0,0 +1,11 @@
+from dataclass import dataclass
+
+@dataclass
+class TestResult:
+    student_number: str
+    test_id: str
+    marks_obtained: int
+    marks_available: int
+
+    def percentage(self) -> float:
+        return (self.marks_obtained / self.marks_available) * 100
